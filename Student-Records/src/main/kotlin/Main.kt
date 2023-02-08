dc9720eadfe67fecb46962b5b33d2d48a2c95390
leilaashur <leilahashur@gmail.com>
2023-02-08 13:45:04 +0300

    first commit

---
+++ Student-Records/src/main/kotlin/Main.kt
@@ -0,0 +1,10 @@
+fun main(args: Array<String>) {
+    var fullname = "leilaashur"
+    var age : Byte = 35
+    var number = 726794700
+    var wheight = 64
+    var kenyan: Boolean = true
+    var notkenyan:Boolean =false
+
+    println(age)
+}
