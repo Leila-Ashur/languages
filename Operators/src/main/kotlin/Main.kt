e82ba32dceb5fc1bb1931cccba125db305e6073d
leilaashur <leilahashur@gmail.com>
2023-02-09 09:36:16 +0300

    first commit

---
+++ Operators/src/main/kotlin/Main.kt
@@ -0,0 +1,20 @@
+fun main(){
+    name()
+
+
+
+    var sum = num1 + num2
+
+    println(sum)
+
+
+}
+    fun greeting(name: String){
+
+        println( "hello "+ name): Int}
+        var sum = num1+num2
+
+o
+     fun radius( :short, y: s)
+
+
